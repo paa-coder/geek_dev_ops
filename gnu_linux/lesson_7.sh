e0a47c4077e26b907734f9bea41e7cf32f60ab50
paa <first_son@list.ru>
2019-12-27 12:23:33 +0400

    init net

---
+++ gnu_linux/lesson_7.sh
@@ -0,0 +1,14 @@
+git init
+git add origin git@github.com:username/new_repo
+git add .
+git commit -m 'some commit'
+git push
+
+git clone git@github.com:username/new_repo ./new_repo
+git checkout branch
+git add .
+git commit -m 'some commit another branch'
+git puch
+git checkout master
+git merge branch
+git push 
